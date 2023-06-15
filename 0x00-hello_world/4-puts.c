ac403613b0066a6e6c1adf6771bb7a29442d62c4
lihandafabius <fabiuslihandaachevi@gmail.com>
2023-06-15 00:54:14 -0700

    C programming

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
-int main(){
-	puts("\"programming is like building a multilingual puzzle\n");
-	return 0;
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle\n");
+	return (0);
 }
 
