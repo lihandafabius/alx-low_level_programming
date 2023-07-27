251d4bdf61be63fb50c80bb149919775779a01c5
lihandafabius <fabiuslihandaachevi@gmail.com>
2023-07-27 08:16:18 -0700

    data structures

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,18 +1,17 @@
 #include <stdio.h>
 #include "lists.h"
-#include <stdlib.h>
 /**
-  * print_list - prints all elements of a list_t list
+  * print_listint - prints all elements of a list_t list
   * @h: singly linked list to print
   * Return: number of nodes in the list
   */
-size_t print_list(const list_t *h)
+size_t print_listint(const listint_t *h)
 {
 	size_t i;
 
 	for (i = 0; h; i++)
 	{
-		printf("[%d] %s\n", h->len, h->str);
+		printf("%d\n", h->n);
 		h = h->next;
 	}
 	return (i);
