7d5ced011d1ddb15647e8eacd181c2d39393b102
lihandafabius <fabiuslihandaachevi@gmail.com>
2023-07-27 09:15:02 -0700

    data structures

--- 0x12-singly_linked_lists/trial.c
+++ 0x12-singly_linked_lists/trial.c
@@ -1,5 +1,5 @@
-#include "lists.h"
 #include <stdio.h>
+#include "lists.h"
 
 /**
  * print_list - print all elements of `list_t` list
@@ -8,17 +8,19 @@
  */
 size_t print_list(const list_t *h)
 {
-	size_t count = 0;
-
+       	size_t count = 0;
+	
 	while (h != NULL)
-	{
-		if (h->str == NULL)
+       	{
+	       	if (h->str == NULL)
 			printf("[0] (nil)\n");
 		else
 			printf("[%u] %s\n", h->len, h->str);
-
+		
 		h = h->next;
 		count++;
-	}
-	return (count);
+       	}
+	
+	return count;
 }
+
