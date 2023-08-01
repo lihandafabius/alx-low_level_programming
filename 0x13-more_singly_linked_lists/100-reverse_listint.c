fd1a0be7dd0f67a52d98745d71ec1f7982e44c52
lihandafabius <fabiuslihandaachevi@gmail.com>
2023-08-01 08:35:04 -0700

    update

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -1,40 +1,43 @@
 #include "lists.h"
-#include <stdio.h>
+#include <stdlib.h>
 
 /**
-  * reverse_listint - reverse function
-  * @head: points to the first head
-  * Return: return value
-  * Description: Limited to going through loop only once.
-  * Not allowed to use `malloc` or `free` or arrays
-  * Can only declare a max of 2 variables.
-  */
+ * reverse_recur - function to reverse the list
+ * @first: first node to reverse
+ * @second: second node to reverse
+ * Return: void
+ */
+listint_t *reverse_recur(listint_t *first, listint_t *second)
+{
+	listint_t *ptr, *prev = NULL;
+ 
+	ptr = first;
+	while (ptr->next != second)
+	{
+		prev = ptr;
+		ptr = ptr->next;
+	}
+
+	if (prev != NULL)
+		prev->next = first;
+	second = first->next;
+	first->next = ptr->next;
+	if (first != ptr && second != first)
+		second = reverse_recur(second, first);
+	ptr->next = second;
+	return (ptr);
+}
+
+/**
+ * reverse_listint - reverses a listint list
+ * @head: points to the first node in the list
+ * Return: success
+ */
 listint_t *reverse_listint(listint_t **head)
 {
-	listint_t *wait;
-	listint_t *curr;
-
-	if (*head == NULL)
+	if (head == NULL || *head == NULL)
 		return (NULL);
 
-	curr = *head;
-	*head = curr->next;
-	wait = (*head)->next;
-	curr->next = NULL;
-	if (*head == NULL)
-	{
-		*head = curr;
-		return (curr);
-	}
-
-	while (wait != NULL)
-	{
-		(*head)->next = curr;
-		curr = *head;
-		*head = wait;
-		wait = (*head)->next;
-	}
-
-	(*head)->next = curr;
+	*head = reverse_recur(*head, NULL);
 	return (*head);
 }
