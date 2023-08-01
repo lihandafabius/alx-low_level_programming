7f7468b2f3f38906997fe9dbb7408779ad74d2a4
lihandafabius <fabiuslihandaachevi@gmail.com>
2023-08-01 07:57:46 -0700

    update

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,5 +1,6 @@
 #include "lists.h"
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
   * print_listint_safe - print function
@@ -8,6 +9,28 @@
   */
 size_t print_listint_safe(const listint_t *head)
 {
-	head++;
-	return (0);
+	const listint_t *curr;
+	size_t count;
+	const listint_t *wait;
+
+	curr = head;
+	if (curr == NULL)
+		exit(98);
+
+	count = 0;
+	while (curr != NULL)
+	{
+		wait = curr;
+		curr = curr->next;
+		count++;
+		printf("[%p] %d\n", (void *)wait, wait->n);
+
+		if (wait < curr)
+		{
+			printf("-> [%p] %d\n", (void *)curr, curr->n);
+			break;
+		}
+	}
+
+	return (count);
 }
