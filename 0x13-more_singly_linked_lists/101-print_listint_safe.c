f6efb0b11947ef13e2afe9516eea24195463afbf
lihandafabius <fabiuslihandaachevi@gmail.com>
2023-08-01 08:26:22 -0700

    update

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -3,11 +3,9 @@
 #include <stdio.h>
 
 /**
- * find_listint_loop_pl - finds a loop in a linked list
- *
- * @head: linked list to search
- *
- * Return: address of node where loop starts/returns, NULL if no loop
+ * find_listint_loop_pl - function to find a loop
+ * @head: points to the first node in the list
+ * Return: success
  */
 listint_t *find_listint_loop_pl(listint_t *head)
 {
@@ -28,30 +26,27 @@ listint_t *find_listint_loop_pl(listint_t *head)
 }
 
 /**
- * print_listint_safe - prints a linked list, even if it
- * has a loop
- *
- * @head: head of list to print
- *
- * Return: number of nodes printed
+ * print_listint_safe - function to print the list
+ * @head: points to the first node in the list
+ * Return: Returns nodes printed
  */
 size_t print_listint_safe(const listint_t *head)
 {
 	size_t len = 0;
-	int loop;
-	listint_t *loopnode;
+	int x;
+	listint_t *loop;
 
-	loopnode = find_listint_loop_pl((listint_t *) head);
+	loop = find_listint_loop_pl((listint_t *) head);
 
-	for (len = 0, loop = 1; (head != loopnode || loop) && head != NULL; len++)
+	for (len = 0, x = 1; (head != loop || x) && head != NULL; len++)
 	{
 		printf("[%p] %d\n", (void *) head, head->n);
-		if (head == loopnode)
-			loop = 0;
+		if (head == loop)
+			x = 0;
 		head = head->next;
 	}
 
-	if (loopnode != NULL)
+	if (loop != NULL)
 		printf("-> [%p] %d\n", (void *) head, head->n);
 	return (len);
 }
