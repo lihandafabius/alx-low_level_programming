860fa99b690766dc0e0d23f856c81906c83a6f85
lihandafabius <fabiuslihandaachevi@gmail.com>
2023-08-01 09:03:08 -0700

    update

--- 0x13-more_singly_linked_lists/103-find_loop.c
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -1,13 +1,25 @@
 #include "lists.h"
-#include <stdio.h>
+#include <stdlib.h>
 
 /**
-  * find_listint_loop - loop  function
-  * @head: first node in the list
-  * Return: success
-  */
+ * find_listint_loop - function to find list
+ * @head: points to first node in the list
+ * Return: success
+ */
 listint_t *find_listint_loop(listint_t *head)
 {
-	head++;
-	return (0);
+	listint_t *ptr, *end;
+
+	if (head == NULL)
+		return (NULL);
+	for (end = head->next; end != NULL; end = end->next)
+	{
+		if (end == end->next)
+			return (end);
+		for (ptr = head; ptr != end; ptr = ptr->next)
+			if (ptr == end->next)
+				return (end->next);
+	}
+
+	return (NULL);
 }
