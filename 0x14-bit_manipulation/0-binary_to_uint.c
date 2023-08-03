dbdf47821c7a76b0de229bd50c82933edb4bcd3f
lihandafabius <fabiuslihandaachevi@gmail.com>
2023-08-03 03:21:48 -0700

    update

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,31 +1,30 @@
-#include <stddef.h>
+#include "main.h"
+#include <stdio.h>
 
 /**
- * binary_to_uint - Function to convert a bit string to unsigned int
- * @b: The string pointed 
- * Return: decimal unsigned int value
+ * binary_to_uint - Function to convert a string to an unsigned int
+ * @b: string to be cinverted
+ * Return: success
  */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int x;
+	unsigned int x, y;
+	int z;
 
 	if (b == NULL)
 		return (0);
-	for (x = 0; *b != 0; b++)
+
+	for (z = 0; b[z]; z++)
 	{
-		if (*b == '0')
-		{
-			x = x << 1;
-		}
-		else if (*b == '1')
-		{
-			x = x << 1;
-			x++;
-		}
-		else
-		{
+		if (b[z] != '0' && b[z] != '1')
 			return (0);
-		}
 	}
+
+	for (y = 1, x = 0, z--; z >= 0; z--, y *= 2)
+	{
+		if (b[z] == '1')
+			x += y;
+	}
+
 	return (x);
 }
